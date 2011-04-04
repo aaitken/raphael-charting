6cd4b71a6d4b56ed27a655dfaa670c6e08747e19
Alex Aitken <aaitken@alexaitken.net>
2011-04-04 17:04:56 -0600

    Circle method replaced with filled path definitions.

--- javascript/_framework/raphael-fbPie.js
+++ javascript/_framework/raphael-fbPie.js
@@ -1,37 +1,42 @@
-if(Raphael.fbPie){alert('conflict');}
+if(Raphael.amaPie){alert('conflict');}
 else{
-    Raphael.fbPie=function(opts){
+    Raphael.amaPie=function(opts){
 
         //private
-        var data=opts.data,//[[int,label]...]
-            dataLength=data.length,
-            dataTotal=0,
-            fbPie,//this is what we return
-            i,
+        var amaPie={},//this is object whose properties we reveal and return
+            data=opts.data,//[[int,label]...]
             paper,
-            prnt=opts.prnt,
-            radi=opts.radi;
+            prnt=opts.prnt,//parent
+            radi=opts.radi;//radius
 
-        //fbPie definition
+        //amaPie definition
         if(prnt.raphael){//Raphael insertion
             console.log('raph');
         }
         else{//DOM insertion
-            fbPie={};
-            fbPie.paper=paper=Raphael(prnt,radi*2,radi*2);
-            fbPie.circle=fbPie.paper.circle(radi,radi,radi);
+            amaPie.paper=paper=Raphael(prnt,radi*2,radi*2);//the raphael 'canvas'
+            amaPie.set=paper.set();//similar to native svg 'g' element - array of Raph objects
+            amaPie.wedges=[];//collection of DOM elements
         }
 
         //pie slicing
         (function(){
 
             //private
-            var L=0,//radian angle
+            var arcFlags,//large-arc-flag and sweep-flag per http://www.w3.org/TR/SVG/paths.html#PathDataEllipticalArcCommands
+                dataLength=data.length,
+                dataTotal=0,
+                dataPct,
+                i,
+                L=0,//radian angle
                 L2=function(mult){return L-(mult*Math.PI);},//translated radian angle per quadrant
-                pth0='M'+radi+' '+radi+'L',
+                pnt0=2*radi+','+radi,
+                pnt1,
+                pth0='M'+radi+','+radi+'L',
                 x,
                 xget=function(angle,trig){x=Math[trig](angle)*radi;return x;},
                 y=function(x){return Math.sqrt(Math.pow(radi,2)-Math.pow(x,2));};//pythag
+
 
             //data total
             for(i=0;i<dataLength;i++){
@@ -40,22 +45,42 @@ else{
             
             //wedges
             for(i=0;i<dataLength;i++){
-                L+=(data[i][0]/dataTotal)*2*Math.PI;//2MathPI = 360 deg in radians
-                if(L<=0.5*Math.PI){//1st quadrant
-                    paper.path(pth0+(radi+xget(L,'cos'))+' '+(radi-y(x)));
-                }
-                else if(L<=Math.PI){//2nd quadrant
-                    paper.path(pth0+(radi-xget(L2(0.5),'sin'))+' '+(radi-y(x)));
-                }
-                else if(L<=1.5*Math.PI){//3rd quadrant
-                    paper.path(pth0+(radi-xget(L2(1),'cos'))+' '+(radi+y(x)));
-                }
-                else{//4th quadrant
-                    paper.path(pth0+(radi+xget(L2(1.5),'sin'))+' '+(radi+y(x)));
-                }
+                dataPct=data[i][0]/dataTotal;
+                arcFlags=dataPct>0.5?'1,1':'0,1';
+                L+=(dataPct)*2*Math.PI;//2MathPI = 360 deg in radians
+                pnt1=(function(){
+                    if(L<=0.5*Math.PI){//1st quadrant
+                        return (radi+xget(L,'cos'))+','+(radi-y(x));
+                    }
+                    else if(L<=Math.PI){//2nd quadrant
+                        return (radi-xget(L2(0.5),'sin'))+','+(radi-y(x));
+                    }
+                    else if(L<=1.5*Math.PI){//3rd quadrant
+                        return (radi-xget(L2(1),'cos'))+','+(radi+y(x));
+                    }
+                    else{//4th quadrant
+                        return (radi+xget(L2(1.5),'sin'))+','+(radi+y(x));
+                    }
+                }());
+                amaPie.set.push(paper.path(pth0+pnt1+'A'+radi+','+radi+' '+'0 '+arcFlags+' '+pnt0+'Z').attr({
+                    stroke:opts.strk,
+                    'stroke-width':opts.wdth,
+                    fill:opts.fill
+                }));
+                amaPie.wedges[i]=amaPie.set[i].node;
+                amaPie.wedges[i].label=data[i];//mutation adds original source data to DOM element
+                pnt0=pnt1;
             }
         }());
 
-        return fbPie;
+        //scale down to avoid clipping and allow room for animation
+        amaPie.set.scale(0.9,0.9,radi,radi);
+
+        //reveal and return
+        return {
+            paper:amaPie.paper,
+            set:amaPie.set,
+            wedges:amaPie.wedges
+        };
     }
 }
--- javascript/individual/raphael-fbPie_all.js
+++ javascript/individual/raphael-fbPie_all.js
@@ -1,13 +1,32 @@
-var fbPie=Raphael.fbPie({
+var fbPie=Raphael.amaPie({
     prnt:document.getElementById('pieChart'),
-    radi:100,
+    radi:170,
+    strk:'#999',
+    wdth:2,
+    fill:'#eee',
     data:[
-        [15,'Internet Explorer'],
-        [200,''],
+        [50,'IE'],
         [50,'Firefox'],
-        [200,''],
-        [50,'Firefox']
+        [20,'Firefox'],
+        [20,'Firefox']
     ]
 });
 
+var center={
+    x:fbPie.set[0].getPointAtLength(fbPie.set[0].getTotalLength()).x,
+    y:fbPie.set[0].getPointAtLength(fbPie.set[0].getTotalLength()).y
+};
+
+for (var i=0;i<fbPie.set.length;i++){
+    fbPie.wedges[i].addEventListener('click',function(){alert(this.label[1]+' - '+this.label[0])},false);
+    fbPie.wedges[i].addEventListener('mouseover',function(){
+
+       fbPie.set[i].scale(.95,.95,center.x,center.y);
+    },false);
+}
+
+/*alert(fbPie.set.length);
+
+fbPie.set[0].scale(.95,.95,fbPie.set[0].getPointAtLength(fbPie.set[0].getTotalLength()).x,fbPie.set[0].getPointAtLength(fbPie.set[0].getTotalLength()).y);
+fbPie.wedges[0].addEventListener('click',function(){alert(this.label[1]+' - '+this.label[0])},false);*/
 
